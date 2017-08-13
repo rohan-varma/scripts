10382fb22fc18cbadc9d053f9f8e3518d98652f1
Rohan Varma <rvarm1@gmail.com>
2017-08-12 19:06:01 -0700

    commit

---
+++ setupgit.sh
@@ -0,0 +1,16 @@
+# does all of the commands for initializing a git repo
+# usage: ./setupgit.sh repo_name (can alias)
+
+GIT_STR='https://github.com/rohan-varma/'
+GIT_END='.git'
+if [ $# -eq 0 ]; then
+	echo "Usage: ./setupgit.sh repo_name"
+	exit 0
+else
+	echo $1
+	git init
+	touch README.md
+	git add .
+	git commit -m "commit"
+	git remote add origin $GIT_STR$1$GIT_END
+fi
