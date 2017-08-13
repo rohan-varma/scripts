59055ace1eb3111a3a62056e695b6f8a33274c8d
Rohan Varma <rvarm1@gmail.com>
2017-08-12 20:02:51 -0700

    update to commit

--- commit.sh
+++ commit.sh
@@ -1,10 +1,8 @@
 # so i don't have to do 3 things to commit and push up some code
 if [ $# -eq 0 ]; then
-    echo "Usage: ./commit.sh COMMIT_MSG (can alias)"
+    echo "Usage: ./commit.sh COMMIT_MSG"
 else
     git add .
-    COMMIT_MSG=$1
-    echo $COMMIT_MSG
-    git commit -m $1
+    git commit -m "$1"
     git push
 fi
