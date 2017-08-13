3546342e0a0ea1592e0a6cd9ee47eab304749f81
Rohan Varma <rvarm1@gmail.com>
2017-08-12 19:13:24 -0700

    add better description

--- nukeonly.sh
+++ nukeonly.sh
@@ -1,4 +1,5 @@
 # Replaces it.only and describe.only occurences with it and describe
+# This is useful if you're writing/testing a bunch of JS code using mocha/chai, and have a bunch of .onlys that you need to remove before committing.
 # If a path is provided, if it's a file then only occurences in that file are changed. If it's a directory that directory's files are recursively evaluated
 # If nothing is provided then the current directory's files are recursively evaluated. 
 
