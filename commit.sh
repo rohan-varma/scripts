f7126d1fd7f9ff07ff6a0baeb7fe050aaf72b587
Rohan Varma <rvarm1@gmail.com>
2017-08-12 19:36:15 -0700

    hi

--- commit.sh
+++ commit.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0 ]; then
     echo "Usage: ./commit.sh COMMIT_MSG (can alias)"
 else
     git add .
-    $COMMIT_MSG=$1
+    COMMIT_MSG=$1
     echo $COMMIT_MSG
     git commit -m $1
     git push
