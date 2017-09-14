e542e34e38dd1da139c58b987ce174cc8d4b1a28
Rohan Varma <rvarm1@gmail.com>
2017-09-14 03:46:45 -0700

    small nits

--- setupgit.sh
+++ setupgit.sh
@@ -5,20 +5,18 @@
 GIT_STR='https://github.com/rohan-varma/'
 GIT_END='.git'
 if [ $# -eq 0 ]; then
-	echo "Usage: ./setupgit.sh repo_name"
+	echo "Usage: ./setupgit.sh repo_name -p"
 	exit 0
 else
-	echo $1
 	git init
-	touch README.md
 	echo "$1" > README.md
 	git add .
 	git commit -m "commit"
 	git remote add origin $GIT_STR$1$GIT_END
 	if [[ $2 == "-p" ]]; then
-		echo "push command given"
+		echo "-p was given, so pushing up the repo"
 		git push -u origin master
 	else
-		echo "no push!"
+		echo "no push command (-p) given, so not pushing up the repo."
 	fi
 fi
